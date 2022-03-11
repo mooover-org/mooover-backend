68b3d5d640971b64d343d1c0ea39f4bfdb0e09ec
Adrian Pop <adi.pop.bv@gmail.com>
2022-03-11 16:45:05 +0200

    MVR-23 Added the docker, docker-compose and nginx configs and tested the setup

--- user_services/main.py
+++ user_services/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 from app.api import router
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/users/openapi.json", docs_url="/api/v1/users/docs")
 
 app.include_router(router, prefix='/api/v1/users', tags=['users'])
